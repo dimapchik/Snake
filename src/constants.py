236c194b07875f5fce10eb66c677a8967c66bd2e
Dmitri Prokopev <Dima02112003Dima@yandex.ru>
2023-04-29 19:28:22 +0300

    fix main_menu interface

--- src/constants.py
+++ src/constants.py
@@ -14,3 +14,21 @@ RIGHT = (1, 0)
 LEFT = (-1, 0)
 UP = (0, -1)
 DOWN = (0, 1)
+
+
+class InterfaceMainMenu:
+    def __init__(self, width, height):
+        self.title_width = width / 2 - 70
+        self.title_height = height / 4
+        self.button_width = width / 3
+        self.first_button_height = height / 3
+        self.second_button_height = self.first_button_height * 3 / 2
+        self.third_button_height = self.first_button_height * 2
+        self.length_button = width * 3 / 8
+        self.thickness = 60
+        self.big_thickness = 85
+        self.shift_height = 15
+        self.start_shift_width = 65
+        self.setting_shift_width = 45
+        self.leader_shift_width = 50
+        self.board_shift_width = 60
--- src/menu.py
+++ src/menu.py
@@ -2,17 +2,32 @@ import pygame
 from src.constants import *
 
 
+class Back:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.main_menu = pygame.Rect(width / 10, height / 10, width / 5, width / 25)
+
+    def draw(self, screen, font):
+        pygame.draw.rect(screen, RED, self.main_menu)
+        main_menu_text = font.render('Back', True, BLACK)
+        screen.blit(main_menu_text, (self.width / 10, self.height / 10))
+
+
 class Menu:
     def __init__(self, width, height):
         self.width = width
         self.height = height
-        self.delta = width / 10
+        self.interface = InterfaceMainMenu(width, height)
         self.screen = pygame.display.set_mode((self.width, self.height))
         self.font = pygame.font.SysFont('Arial', 30)
         self.title = self.font.render('Snake Game', True, BLACK)
-        self.start_button = pygame.Rect(self.width / 3, self.height / 3, self.width * 3 / 8, self.delta)
-        self.settings_button = pygame.Rect(self.width / 3, self.height / 2, self.width * 3 / 8, self.delta)
-        self.leader_board_button = pygame.Rect(self.width / 3, self.height * 2 / 3, self.width * 3 / 8, 2 * self.delta)
+        self.start_button = pygame.Rect(self.interface.button_width, self.interface.first_button_height,
+                                        self.interface.length_button, self.interface.thickness)
+        self.settings_button = pygame.Rect(self.interface.button_width, self.interface.second_button_height,
+                                           self.interface.length_button, self.interface.thickness)
+        self.leader_board_button = pygame.Rect(self.interface.button_width, self.interface.third_button_height,
+                                               self.interface.length_button, self.interface.big_thickness)
 
     def draw(self):
         self.screen.fill(WHITE)
@@ -22,10 +37,16 @@ class Menu:
         pygame.draw.rect(self.screen, BLUE, self.leader_board_button)
         start_text = self.font.render('Start', True, BLACK)
         settings_text = self.font.render('Settings', True, BLACK)
-        leader_board_text = self.font.render("Leader \n board", True, BLACK)
-        self.screen.blit(start_text, (self.width / 3 + self.delta, self.height / 3 + self.delta / 5))
-        self.screen.blit(settings_text, (self.width / 3 + self.delta, self.height / 2 + self.delta / 5))
-        self.screen.blit(leader_board_text, (self.width / 3 + self.delta, self.height * 2 / 3 + self.delta / 5))
+        leader_text = self.font.render("Leader", True, BLACK)
+        board_text = self.font.render("board", True, BLACK)
+        self.screen.blit(start_text, (self.interface.button_width + self.interface.start_shift_width,
+                                      self.interface.first_button_height + self.interface.shift_height))
+        self.screen.blit(settings_text, (self.interface.button_width + self.interface.setting_shift_width,
+                                         self.interface.second_button_height + self.interface.shift_height))
+        self.screen.blit(leader_text, (self.interface.button_width + self.interface.leader_shift_width,
+                                       self.interface.third_button_height + self.interface.shift_height))
+        self.screen.blit(board_text, (self.interface.button_width + self.interface.board_shift_width,
+                                      self.interface.third_button_height + 3 * self.interface.shift_height))
         pygame.display.update()
 
 
@@ -65,6 +86,7 @@ class Setting:
         self.level_buttons = []
         self.count_levels = 5
         self.main_menu = pygame.Rect(self.width / 10, height / 10, self.width / 5, width / 25)
+        self.back = Back(width, height)
         for i in range(self.count_levels):
             self.level_buttons.append(pygame.Rect(self.width * (i + 1) / (self.count_levels + 1), self.height * 2 / 3,
                                                   width / 10, width / 10))
@@ -76,9 +98,7 @@ class Setting:
             pygame.draw.rect(self.screen, GREEN, self.level_buttons[i])
             level_text = self.font.render(f'{i + 1} level', True, BLACK)
             self.screen.blit(level_text, (self.width * (i + 1) / (self.count_levels + 1), self.height * 2 / 3))
-        pygame.draw.rect(self.screen, RED, self.main_menu)
-        main_menu_text = self.font.render('Back', True, BLACK)
-        self.screen.blit(main_menu_text, (self.width / 10 + self.delta, self.height / 10 + self.delta / 5))
+        self.back.draw(self.screen, self.font)
         pygame.display.update()
 
 
@@ -92,6 +112,7 @@ class LeaderBoard:
         self.title = self.font.render('Leader Board', True, BLACK)
         self.main_menu = pygame.Rect(self.width / 10, height / 10, self.width / 5, width / 25)
         self.positions = dict()
+        self.back = Back(width, height)
         for i in range(1, 6):
             self.positions['position_%s' % i] = pygame.Rect(self.width / 3, self.height / 2 + self.height * i / 5,
                                                             self.width / 4, width / 25)
@@ -113,7 +134,5 @@ class LeaderBoard:
             (name, score) = top[i - 1]
             position_text = self.font.render(str(name) + ': ' + str(score), True, BLACK)
             self.screen.blit(position_text, (self.width / 3, self.height / 2 + self.height * i / 5))
-        pygame.draw.rect(self.screen, RED, self.main_menu)
-        main_menu_text = self.font.render('Back', True, BLACK)
-        self.screen.blit(main_menu_text, (self.width / 10 + self.delta, self.height / 10 + self.delta / 5))
+        self.back.draw(self.screen, self.font)
         pygame.display.update()
